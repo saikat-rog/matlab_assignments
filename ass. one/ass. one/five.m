e3648799cf32977061f14eccce6e850d655cb9c5
Saikat Das <97933273+saikat-rog@users.noreply.github.com>
2023-08-07 03:33:09 +0530

    Delete ass. one/ass. one directory

--- ass. one/ass. one/five.m
+++
@@ -1,9 +0,0 @@
-%{
-Sure, here's a short explanation of the use of each file type in MATLAB:
-
-1. `.m` files: These are MATLAB script files that contain code to be executed. They are used to write and run MATLAB programs, including scripts and functions.
-
-2. `.mat` files: These are binary data files used to store variables and data in MATLAB. They are useful for saving and loading large datasets or complex variables for later use.
-
-3. `.fig` files: These are binary files used to save MATLAB figures and graphical user interfaces (GUIs). They preserve the appearance and interactive elements of plots and GUIs for reuse or modification.
-%}
